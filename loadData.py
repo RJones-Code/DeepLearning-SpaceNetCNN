2e206a76860c2213c24f38ff19ce8e08c97ec34f
PlatinumFrog <teddyvboswell@gmail.com>
2025-11-09 12:16:56 -0500

    class outline for loading data

---
+++ loadData.py
@@ -0,0 +1,10 @@
+import torch
+
+class LoadData:
+    def __init__(self, fileString):
+        self.fileName = fileString
+        self.data
+    def getSingle():
+        pass
+    def getRandomBatch(size):
+        pass
